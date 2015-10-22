14ef30e394885db2f971deec34a3e283d9968aba
Mariusz Smykula <mariuszs@gmail.com>
2015-10-22 13:30:11 +0200

    initial commit

---
+++ src/main/groovy/io/codearte/converter/ConverterApplication.groovy
@@ -0,0 +1,12 @@
+package io.codearte.converter
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ConverterApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run ConverterApplication, args
+    }
+}
